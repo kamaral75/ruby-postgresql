eadd7fb457dbdf9d85843d5686519e7d293f6603
kamaral75 <kamaral@broadinstitute.org>
2019-04-14 00:45:44 -0400

    Build script

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+ENV=DEV
 
-docker-compose run web bundle install
+if [ -z "${ENV}" ]; then
+    echo "ENV variable not set.  Exiting!"
+    exit 1
+fi
+
+[ "$ENV" = PROD ] &&
+   docker-compose run web bundle install ||
+   docker-compose run web bundle install --without production
